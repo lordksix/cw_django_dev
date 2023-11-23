3bc463aa21698031c9316f7b1e81f18b7d364387
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-11-23 16:54:43 -0500

    Implemente filters tags for answers

--- survey/templatetags/my_filters.py
+++ survey/templatetags/my_filters.py
@@ -13,3 +13,18 @@ def get_like_user(query: Question, user):
 @register.filter(name="get_dislike_user")
 def get_dislike_user(query: Question, user):
     return query.user_dislike(user)
+
+
+@register.filter(name="custom_range")
+def custom_range(min=6):
+    return range(1, min)
+
+
+@register.filter(name="get_answer_user")
+def get_answer_user(query: Question, user):
+    return query.user_value(user)
+
+
+@register.filter(name="is_gte")
+def is_gte(current_value, test_value):
+    return current_value >= test_value
